44b086cdfcf2aedea3fd7ddfdf6955aa1c5be938
iancrystal <ian_crystal@yahoo.com>
2011-04-04 23:56:24 -0700

    also validate customer_email

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,7 +3,7 @@ class Job < ActiveRecord::Base
   has_one :category
   acts_as_textiled :description
 
-  validates_presence_of :category_id, :company, :city, :state, :title, :contact_info
+  validates_presence_of :customer_email, :category_id, :company, :city, :state, :title, :contact_info
   
   def self.state_array
     %w{ AK AL AR AK CA CO CT DE FL GA HI ID IL IN IA KS KN LA MA MD ME MI MN MO MS MT NC ND NE NH NJ NM NV NY OH OK OR PA RI SC SD TN TX UT VT VA WA WI WV WY Other }

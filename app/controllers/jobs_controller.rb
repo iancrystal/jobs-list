3d4aa01f9af9c1c436ed23122eda9bafb3671c63
iancrystal <ian_crystal@yahoo.com>
2011-04-07 09:45:11 -0700

    go to payment page after submit

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -85,8 +85,9 @@ class JobsController < ApplicationController
     respond_to do |format|
       if @job.save
         # email the customer including the payment instructions
-        JobPostMailer.deliver_confirm_post(@job, "#{request.protocol}#{request.host}:#{request.port}/payment/index/#{@job.id}")
-        format.html { redirect_to(@job, :notice => 'Job was successfully created.') }
+        #JobPostMailer.deliver_confirm_post(@job, "#{request.protocol}#{request.host}:#{request.port}/payment/index/#{@job.id}")
+        flash[:notice] = "Your job posting titled \"#{@job.title}\" was successfully created. Please pay for the posting to be activated."
+        format.html { redirect_to(:controller => "payment", :action => "index", :id => @job.id) }
         format.xml  { render :xml => @job, :status => :created, :location => @job }
       else
         @category_array = Category.category_array
